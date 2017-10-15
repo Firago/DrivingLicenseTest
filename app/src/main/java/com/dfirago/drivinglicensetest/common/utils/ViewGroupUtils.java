8c4cc4d954254f45e1e328785800e6ccd4033677
Dmytro Firago <firago94@gmail.com>
2017-10-15 10:32:03 +0200

    Copy layout params when replacing a view; Replace merge tag with actual layouts in order to properly perform replacement

--- app/src/main/java/com/dfirago/drivinglicensetest/common/utils/ViewGroupUtils.java
+++ app/src/main/java/com/dfirago/drivinglicensetest/common/utils/ViewGroupUtils.java
@@ -20,6 +20,7 @@ public class ViewGroupUtils {
     }
 
     public static void replaceView(View currentView, View newView) {
+        newView.setLayoutParams(currentView.getLayoutParams());
         ViewGroup parent = getParent(currentView);
         if (parent != null) {
             final int index = parent.indexOfChild(currentView);
